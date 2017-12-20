fd4572a719dc0d1c7e576accf76bf3fffc32e52c
ik012982i9 <ik012982i9@206-05-HZ055291.H012982.NET>
2017-12-20 12:27:35 +0100

    dos nuevas clases:
    - OsoPanda
    - MainPanda

---
+++ src/zoo/MainPanda.java
@@ -0,0 +1,13 @@
+package zoo;
+
+public class MainPanda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		OsoPanda panda = new OsoPanda("Mini", 10); 
+		
+		panda.mostrar();
+	}
+
+}
