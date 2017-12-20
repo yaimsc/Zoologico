d7a16d2c3cb5eac7110e2d5c8d1416248687c0f3
ik012982i9 <ik012982i9@206-23-HZ109059.H012982.NET>
2017-12-20 12:34:28 +0100

    perro

--- src/zoo/Perro.java
+++ src/zoo/Perro.java
@@ -1,7 +1,30 @@
 package zoo;
 
-public class Perro {
+public class Perro extends Animal {
 
+
+
+	
+	public Perro(String especie, int patas) {
+		super(especie, patas);
+	}
+	
 	private String raza;
 	private String color;
+	
+	
+	
+	
+	public String getRaza() {
+		return raza;
+	}
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
 }
