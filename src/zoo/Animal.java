c614c2984c167739cdd9888e2c4c22603f9b36eb
enaut <IR012982EX@206-01-HZ055282.H012982.NET>
2017-12-20 11:55:57 +0100

    primer commit con constructor y getters y setters

---
+++ src/zoo/Animal.java
@@ -0,0 +1,46 @@
+package zoo;
+
+public class Animal {
+	
+	private String especie;
+	private int patas;
+	
+	public Animal(String especie, int patas) {
+		this.especie = especie;
+		this.patas = patas;
+	}
+	
+	public void mostrar(){
+		System.out.println("Soy " + this.especie + " y tengo " + patas + " patas");
+	}
+
+	/**
+	 * @return the especie
+	 */
+	public String getEspecie() {
+		return especie;
+	}
+
+	/**
+	 * @param especie the especie to set
+	 */
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+
+	/**
+	 * @return the patas
+	 */
+	public int getPatas() {
+		return patas;
+	}
+
+	/**
+	 * @param patas the patas to set
+	 */
+	public void setPatas(int patas) {
+		this.patas = patas;
+	}
+	
+	
+}
