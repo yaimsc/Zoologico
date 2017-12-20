aa4d150a782a5621fd262335d7b5474f2bf1bc99
ik012982i9 <ik012982i9@206-18-HZ055316.H012982.NET>
2017-12-20 12:36:54 +0100

    Añadir rana

---
+++ src/zoo/MainRana.java
@@ -0,0 +1,12 @@
+package zoo;
+
+public class MainRana {
+
+	public static void main(String[] args) {
+		Rana r1 = new Rana();
+		Rana r2 = new Rana("Saltarina",4,"Rojo");
+		
+		r2.mostrarInfo();
+	}
+
+}
