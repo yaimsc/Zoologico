4ee16d520ba203fe2ab3b7c0de07c819eda31932
Josu <ik012982i9@206-09-HZ108349.H012982.NET>
2017-12-22 13:08:41 +0100

    Añadir el Main

--- src/zoo/Main_JosuG.java
+++ src/zoo/Main_JosuG.java
@@ -1,11 +1,10 @@
-package vehiculos;
+package animaleslocal;
 
 public class Main_JosuG {
-
-	public static void main(String[] args) {
-		Animal[] animales = new Animal[5];
-		animales[0] = new Elefante();
-
-	}
-
+	Animal[] animales = new Animal[5];
+	animales[0] = new Elefante();
+	animales[1] = new Lombriz();
+	animales[2] = new Mono();
+	animales[3] = new Leon();
+	animales[4] = new Panda();
 }
