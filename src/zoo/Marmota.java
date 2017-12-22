3446a9e27af08dee774016f4572d13d3cb5762e1
Asier F <ik012982i9@206-26-HZ055311.H012982.NET>
2017-12-22 13:20:59 +0100

    Primer commit marmota

---
+++ src/zoo/Marmota.java
@@ -0,0 +1,48 @@
+package zoo;
+
+public class Marmota extends Animal{
+	
+	private int peso;
+	private String nombre;
+	
+	/*
+	 * GETTERS & SETTERS
+	 */
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	
+	/*
+	 * Functions
+	 */
+	
+	public void mostrarinfo(){
+		System.out.println("El " + super.getEspecie() +" tiene " + this.getPeso() +" kilos");
+	}
+	
+
+	
+	/*
+	 * CONSTRUCTORS
+	 */
+	public Marmota( String tipo, int patas ) {
+		super("marota", 4);
+		this.setPeso(peso);
+	}
+	
+}
