102324cb63c5e8f5e6d425e5d2e56e028e3c5e5d
ik012982i9 <ik012982i9@206-05-HZ055291.H012982.NET>
2017-12-22 13:06:29 +0100

    añadir array de animales

--- src/zoo/MainYaizaM.java
+++ src/zoo/MainYaizaM.java
@@ -8,6 +8,16 @@ public class MainYaizaM {
 			Animal[] animales = new Animal[5]; 
 			
 			animales[0] = new OsoPanda("Mini", 10); 
+			
+			animales[1] = new Pinguino("Pingui", 4); 
+			
+			animales[2] = new Lombriz(120); 
+			
+			animales[3] = new Rana(); 
+			
+			animales[4] = new Elefante("Trompy",12); 
+			
+			
 	}
 
 }
