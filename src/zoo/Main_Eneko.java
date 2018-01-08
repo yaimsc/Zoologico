6fa2e4bfee70aa330896a73fe88309c35012657e
Enekovaz4 <Enekovaz4@gmail.com>
2018-01-08 08:31:58 +0100

    Arreglada la clase Gekko (añadido el método Abstract de la clase Animales) + Ej. Lista de animales

--- src/zoo/Main_Eneko.java
+++ src/zoo/Main_Eneko.java
@@ -5,15 +5,15 @@ public class Main_Eneko {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Animal[] animales = new Animal[5];
-		animales[0] = new Elefante();
+		animales[0] = new Pez();
 		animales[1] = new Perro("Ona", "chiuaua");
-		animales[2] = new Leon(150, 1, 6);
-		animales[3] = new Pinguino();
+		animales[2] = new OsoPanda();
+		animales[3] = new Mono();
 		animales[4] = new Gekko();
 		
 		for (int i = 0; i < animales.length; i++){
 			System.out.println(animales[i].getEspecie());
+			animales[i].mostrar();
 		}
 	}
-
 }
